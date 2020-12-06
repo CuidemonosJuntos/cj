471f787b5e167c865d80184fc35950fbacde7957
Paola Garay <garay.angela@gmail.com>
2020-12-06 18:31:45 -0500

    Build the header part 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -9,10 +10,23 @@ export default function Home() {
       </Head>
 
       <header>
+          <nav>
+            <div className="row">
+                <Image className="logo" src="/img/whitelogo.png" alt="logo" width="100px" height="100px" />
+                <ul className="main-nav">
+                        <li><a href="#">Quiénes Somos</a></li>
+                        <li><a href="#">Lo que ofrecemos</a></li>
+                        <li><a href="#">Planes</a></li>
+                        <li><a href="#">Contacto</a></li>
+                        <li><a href="#">Log In</a></li>
+                </ul>
+            </div>
+
+          </nav>
           <div className="hero-text-box">
-              <h1>Transforma Tú Vida <br></br>Aprende de las Culturas Ancestrales</h1>
-              <a className="btn btn-full" href="#">Regístrate</a>
-              <a className="btn btn-ghost" href="https://form.typeform.com/to/VOmPP1iL">Autodiagnóstico</a>
+                <h1>Transforma Tú Vida<br></br>Aprende de las Culturas Ancestrales</h1>
+                <a className="btn btn-full" href="#">Regístrate</a>
+                <a className="btn btn-ghost" href="https://form.typeform.com/to/VOmPP1iL">Autodiagnóstico</a>
           </div>
       </header>
 
@@ -20,12 +34,3 @@ export default function Home() {
   )
 }
 
-
-
-{/* <div className="page-container-welcome">
-
-<div className="content-wrap">
-   Hello dear!
-</div>
-
-</div> */}
