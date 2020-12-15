d465303e5402a02c9c38199cd04b7ce067cca43f
Paola Garay <garay.angela@gmail.com>
2020-12-14 20:41:48 -0500

    add styles into the Accordion component

--- components/shared/Accordion.tsx
+++ components/shared/Accordion.tsx
@@ -15,16 +15,17 @@ export default function Accordion({items}: ServicesProps) {
     }
 
     const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
         return (
             <React.Fragment key={index}>
                 <div
-                    className="title active"
+                    className={`title ${active}`}
                     onClick={() => onTitleClick(index)}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
